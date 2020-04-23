b4e212e3589c1bf16ddf7d3b6ebb42c0c6399772
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2020-04-23 13:04:37 +0300

    Add existing tables check functionality

--- AWS/files/views.py
+++ AWS/files/views.py
@@ -1,11 +1,10 @@
 from django.views.generic import CreateView
 from .models import Upload
 import boto3
-from django.shortcuts import redirect
 import time
 
-from time import gmtime, strftime
 dynamodb = boto3.resource('dynamodb', region_name='eu-west-3')
+client = boto3.client('dynamodb', region_name='eu-west-3')
 
 
 class UploadView(CreateView):
@@ -17,28 +16,31 @@ class UploadView(CreateView):
 
 def dynamoDB(request):
     table_name = request.POST['table_name']
-    table = dynamodb.create_table(
-        TableName=table_name,
-        KeySchema=[
-            {
-                'AttributeName': 'name',
-                'KeyType': 'HASH'  
-            },
-        ],
-        AttributeDefinitions=[
-            {
-                'AttributeName': 'name',
-                'AttributeType': 'S'
-            },
 
-        ],
-        ProvisionedThroughput={
-            'ReadCapacityUnits': 10,
-            'WriteCapacityUnits': 10
-        }
-    )
+    existing_tables = client.list_tables()['TableNames']
 
-    time.sleep(5)
+    if table_name not in existing_tables:
+        table = dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    'AttributeName': 'name',
+                    'KeyType': 'HASH'
+                },
+            ],
+            AttributeDefinitions=[
+                {
+                    'AttributeName': 'name',
+                    'AttributeType': 'S'
+                },
+
+            ],
+            ProvisionedThroughput={
+                'ReadCapacityUnits': 10,
+                'WriteCapacityUnits': 10
+            }
+        )
+        time.sleep(5)
 
     table = dynamodb.Table(table_name)
 
@@ -54,8 +56,8 @@ def dynamoDB(request):
                 'name': file_name,
                 'extension': file_extension,
                 'size': request.FILES['file'].size,
-                'upload date and time': strftime("%Y-%m-%d %H:%M:%S", gmtime())
+                'upload date and time': time.ctime()
             }
     )
 
-    return redirect('/')
+    return UploadView.as_view()(request)
