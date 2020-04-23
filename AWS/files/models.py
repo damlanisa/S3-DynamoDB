172729163941dc881e6cb0abacfbce9da0aa94c5
Damla Nisa CEVIK <damlanisa.cevik@gmail.com>
2020-04-23 13:00:15 +0300

    Add default name to table_name field

--- AWS/files/models.py
+++ AWS/files/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Upload(models.Model):
-    file = models.FileField(upload_to='uploaded_files', max_length=500)
-    table_name = models.CharField(max_length=100) 
+    file = models.FileField(upload_to='uploaded_files')
+    table_name = models.CharField(default="table", max_length=100)
     upload_time = models.DateTimeField(auto_now_add=True)
